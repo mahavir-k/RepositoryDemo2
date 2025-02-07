47850964362800fad2ae9c320f61ba325fb0083d
mahavir-k <mahaviryadav004@gmail.com>
2025-02-07 19:58:41 +0530

    second commit

--- RepositoryDemo2Project/src/Demo2ProjectTest.java
+++ RepositoryDemo2Project/src/Demo2ProjectTest.java
@@ -1,5 +1,6 @@
 public class Demo2ProjectTest {
     public static void main(String[] args) {
         System.out.println("Demo2 test..............!");
+        System.out.println("Demo2 project branch test..............!");
     }
 }
